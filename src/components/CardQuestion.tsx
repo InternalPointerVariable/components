07f6bbac2cb364fbb8b691323440b491944f3d45
adiluexe <adiluexe@gmail.com>
2025-02-22 16:20:26 +0800

    Add CardQuestion component

---
+++ src/components/CardQuestion.tsx
@@ -0,0 +1,9 @@
+export function CardQuestion() {
+    return (
+        <>
+            <div data-layer="CardQuestion" className="Cardquestion h-[54px] p-3 bg-[#5bbea9] rounded-lg justify-start items-center gap-3 inline-flex">
+                <div data-layer="How often should we check on you during an active disaster?" className="HowOftenShouldWeCheckOnYouDuringAnActiveDisaster grow shrink basis-0 text-white text-[10px] font-normal font-['Poppins']">How often should we check on you during an active disaster?</div>
+            </div>
+        </>
+    )
+}
