28163f430ed8c8a738a5e06322bf52730270e415
adiluexe <adiluexe@gmail.com>
2025-02-22 16:16:26 +0800

    Add components folder ; test ButtonLarge

---
+++ src/components/ButtonLarge.tsx
@@ -0,0 +1,9 @@
+export function ButtonLarge() {
+    return (
+        <>
+        <div data-layer="ButtonLarge" className="ButtonLarge" style={{width: 300, height: 48, paddingTop: 12, paddingBottom: 12, background: '#5BBEA9', borderRadius: 8, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div data-layer="Register" className="Register" style={{textAlign: 'center', color: '#FBFBFE', fontSize: 16, fontFamily: 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Register</div>
+        </div>
+        </>
+    )
+}
